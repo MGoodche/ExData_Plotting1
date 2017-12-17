695e4924ecc459ae31d1623f340578fc594194b2
Miriam Pavón Buenache <mirriam.upm@gmail.com>
2017-12-17 12:24:52 +0100

    Update Plot3.R

--- Codes/Plot3.R
+++ Codes/Plot3.R
@@ -25,7 +25,10 @@ data$Date <- as.Date(data$Date, "%d/%m/%Y")
 dates <- as.Date(c("2007-02-01", "2007-02-02"), "%Y-%m-%d")
 data <- subset(data, Date %in% dates)
 ## You can simply make a plot by using the plot() function
-plot(data$Time, data$Global_active_power, type="l", col="black", xlab="", ylab="Global Active Power (kilowatts)")
+plot(data$Time, data$Sub_metering_1, type="l", col="black",xlab="", ylab="Energy sub metering")
+    lines(data$Time, data$Sub_metering_2, col="red")
+    lines(data$Time, data$Sub_metering_3, col="blue")
+    legend("topright", col=c("black", "red", "blue"),c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lty=1,lwd = 2)
 ## Copy the plot to a plot3.PNG file
 dev.copy(png, file = "plot3.png" ,width = 500, height = 500)  
 dev.off()
